59d3b1f69a5e039694c2957d4db5d01e349b23b1
FreeMan <you@example.com>
2025-05-01 11:17:57 +0300

    123123

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/project-bolt-webPorfol/',  // Замените на имя вашего репозитория
+  base: './',  // Замените на имя вашего репозитория
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
